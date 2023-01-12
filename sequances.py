6a3d7ddae4b59a341be2ccb5cd8575a1dda5244e
Minyo <minyolearner@gmail.com>
2023-01-13 00:28:32 +0600

    List & Sequence aded

---
+++ sequances.py
@@ -0,0 +1,18 @@
+name = "Minyo"
+
+print(name[0])
+print(name[1])
+
+
+names = ["Minyo","Momo","Nayeon","jihyo"]
+
+print(names)
+print(names[3])
+
+
+# 2 or 3 Pair value - tuple
+
+coordinateX = 10.0
+coordinateY = 20.0
+
+coordinate = (coordinateX,coordinateY)
