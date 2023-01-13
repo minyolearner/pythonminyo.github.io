df11fcb8d26838f864c83cb600dcd747784d4167
Minyo <minyolearner@gmail.com>
2023-01-13 14:52:12 +0600

    decorator.py .. cs50w

--- decorator.py
+++ decorator.py
@@ -2,3 +2,16 @@
 # Taking Function & Modifing Function - adding some additional behavior to that function
 # decorator is going to be function that takes a function of input and return a modified version of that function as output
 # Some other language function can't be modified  
+
+def announce(f):
+    def wrapper():
+        print("This function going to run...")
+        f()
+        print("Done with the funtion..")
+    return wrapper 
+
+@announce
+def hello():
+    print("Hello, World!")
+
+hello()
