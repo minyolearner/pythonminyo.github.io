aef9e38bc3a6ae5e037cb19d7c855c326280b91c
Minyo <minyolearner@gmail.com>
2023-01-13 11:57:36 +0600

    loops.py created & practice

---
+++ loops.py
@@ -0,0 +1,23 @@
+
+
+# for i in [0,1,2,3,4,5]:
+#     print(i)
+
+# m = [0,1,2,3,4,5] #list
+
+# for m in m:
+#     print(m)
+
+# for i in range(20): # it will print 0 to 19 not 20
+#     print(i + " item")
+
+
+names = ['Momo ','Jihyo','Twice','Nayeon']
+
+for n in names:
+    print(n)
+
+name = "Tyuzu"
+
+for i in name: 
+    print(i)
