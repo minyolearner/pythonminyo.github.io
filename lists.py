4d88f792351268c95e2e3c7446929e4e20f114f2
Minyo <minyolearner@gmail.com>
2023-01-13 11:28:24 +0600

    Python list done and sets.py created

---
+++ lists.py
@@ -0,0 +1,16 @@
+# List - value of unmutable value
+
+names = ['Momo ','Jihyo','Twice','Shiba Inu']
+names[3] = 'Mina'
+
+print(names)
+
+# append a new item.. 
+names.append('Nayeon')
+
+print(names)
+
+# its will change order
+names.sort()
+
+print(names)
---
+++ sets.py
@@ -0,0 +1,1 @@
+# Sets unmutable values
