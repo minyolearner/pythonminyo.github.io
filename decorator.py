10edfd17e737b9674b0b64d0eaeb10d0667b8634
Minyo <minyolearner@gmail.com>
2023-01-13 14:43:43 +0600

    Functional Programming Paradigm Note

---
+++ decorator.py
@@ -0,0 +1,4 @@
+# Starting Functional Programming Paradigm
+# Taking Function & Modifing Function - adding some additional behavior to that function
+# decorator is going to be function that takes a function of input and return a modified version of that function as output
+# Some other language function can't be modified  
