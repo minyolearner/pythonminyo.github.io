8230d8e865fad4d8814b5a97800ff5a1b75fabf7
Minyo <minyolearner@gmail.com>
2023-01-13 12:07:24 +0600

    dict.py - cs50w

---
+++ dict.py
@@ -0,0 +1,15 @@
+# Creating a dictionary
+
+MyFavKPOP ={"Jihyo":"Twice","Jiso":"Blackpink","Tyuzu":"Twice","Haerin":"New Jeans","Yuna":"Itzy"}
+
+# Add new element
+MyFavKPOP["Sana"] = "Twice"
+
+# Change element
+
+MyFavKPOP["Jiso"] = "Twice"
+
+
+print(MyFavKPOP["Jihyo"])
+
+print(MyFavKPOP)
